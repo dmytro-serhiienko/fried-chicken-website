d9dee1487cf14b7dda30024350e0960230e5e346
Dmytro Serhiienko <conneciones@gmail.com>
2025-12-09 20:48:10 +0200

    work

--- js/main.js
+++ js/main.js
@@ -27,9 +27,72 @@ const linkAction = () => {
 navLink.forEach((link) => link.addEventListener("click", linkAction));
 
 /*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = document.getElementById("scroll-up");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+});
+
+scrollUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute("id");
+    const sectionsClass = document.querySelector(
+      '.nav-menu a[href*="' + sectionId + '"]'
+    );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 200,
+});
+
+sr.reveal(".home__data, .about__data, .section__title", {
+  origin: "left",
+  interval: 100,
+});
+
+sr.reveal(".home__images, .about__images", {
+  origin: "right",
+  interval: 100,
+});
+
+sr.reveal(".order__card, .combo__content, .contact__data, .contact__info", {
+  interval: 200,
+});
